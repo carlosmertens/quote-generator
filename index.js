67999670dd48b959760dc08060f8cbeb2bb9f74e
Carlos Mertens <mertenscarlos@yahoo.com>
2021-09-18 18:00:54 +0200

    Add call to local API in case of error

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 let apiQuotes = [];
 
 // Function to randonly choose a single quote
-function getSingleQuote() {
+function getSingleQuote(arrayQuotes) {
   // Pick a random quote using built-in Math function
-  const singleQuote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  const singleQuote =
+    arrayQuotes[Math.floor(Math.random() * arrayQuotes.length)];
   console.log(singleQuote);
+  console.log(arrayQuotes.length);
 }
 
-// Function to get quotes from API
+// Function to get quotes from external API or local API
 async function getApiQuotes() {
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
-    getSingleQuote();
+    getSingleQuote(apiQuotes);
   } catch (error) {
-    // Do something with the error
+    if (error) {
+      getSingleQuote(localQuotes);
+    }
   }
 }
 
