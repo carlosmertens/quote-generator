11b88dcd39395af407b36517b530799a7dbf06ce
Carlos Mertens <mertenscarlos@yahoo.com>
2021-09-18 19:07:43 +0200

    Add statement to check the lenght of the quote to style

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const quoteAuthor = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuote = document.getElementById('new-quote');
+
 let apiQuotes = [];
 
 // Function to randonly choose a single quote
@@ -5,8 +11,19 @@ function getSingleQuote(arrayQuotes) {
   // Pick a random quote using built-in Math function
   const singleQuote =
     arrayQuotes[Math.floor(Math.random() * arrayQuotes.length)];
-  console.log(singleQuote);
-  console.log(arrayQuotes.length);
+  // Check if there is an author
+  if (!singleQuote.author) {
+    quoteAuthor.textContent = 'Unknown';
+  } else {
+    quoteAuthor.textContent = singleQuote['author'];
+  }
+  // Check the lenght of the quote to set styling
+  if (singleQuote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+  quoteText.textContent = singleQuote['text'];
 }
 
 // Function to get quotes from external API or local API
