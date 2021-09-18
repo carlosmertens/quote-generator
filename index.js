1b41de568a5ce28f20da3815395980c869696707
Carlos Mertens <mertenscarlos@yahoo.com>
2021-09-18 17:44:36 +0200

    Create functions to call external API

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("testing");
+let apiQuotes = [];
+
+// Function to randonly choose a single quote
+function getSingleQuote() {
+  // Pick a random quote using built-in Math function
+  const singleQuote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(singleQuote);
+}
+
+// Function to get quotes from API
+async function getApiQuotes() {
+  const apiUrl = 'https://type.fit/api/quotes';
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    getSingleQuote();
+  } catch (error) {
+    // Do something with the error
+  }
+}
+
+// On page load
+getApiQuotes();
